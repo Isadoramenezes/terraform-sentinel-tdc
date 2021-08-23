326de734074151690b51edf6e90fe349f4136343
isadora.menezes <isadora.menezes@netbr.com.br>
2021-08-23 14:49:37 -0300

    demo2

---
+++ firewall/variable.tf
@@ -0,0 +1,17 @@
+
+variable "ufw" {
+    default = "Nginx HTTP"
+}
+
+variable "name_frw" {
+}
+
+variable "source_ranges" {
+}
+
+variable "allow_ports" {
+
+}
+variable "network" {
+  
+}
---
+++ terrafile.tf
@@ -0,0 +1,22 @@
+module "instances" {
+  source        = "./instance"
+  instances     = 2
+  ufw           = var.ufw
+  name_frw      = "firewall-demo-2"
+  allow_ports   = var.allow_ports
+  source_ranges = var.source_ranges
+  depends_on = [
+    google_compute_network.vpc_demo_2
+  ]
+}
+
+module "firewall" {
+  source        = "./firewall"
+  name_frw      = "firewall-demo-2"
+  allow_ports   = var.allow_ports
+  source_ranges = var.source_ranges
+  network       = "vpc-demo-2"
+  depends_on = [
+    google_compute_network.vpc_demo_2
+  ]
+}
